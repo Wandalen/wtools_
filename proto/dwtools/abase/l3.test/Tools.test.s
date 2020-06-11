fc150ee25d09cf8e36d1de2ffedb84ab260b316b
Dmytro <dm.vict.kr@gmail.com>
2020-06-11 15:31:40 +0300

    Add files

---
+++ proto/dwtools/abase/l3.test/Tools.test.s
@@ -0,0 +1,61 @@
+( function _Path_test_s_() {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( '../../Tools.s' );
+
+  _.include( 'wTesting' );
+
+  require( '../../abase/l3/Include.s' );
+
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+// --
+// tests
+// --
+
+function routinesOfTools( test )
+{
+  test.case = 'routines _.array*';
+  test.is( _.routineIs( _.arrayIs ) );
+  test.is( _.routineIs( _.arrayRemove ) );
+
+  test.case = 'routines _.map*';
+  test.is( _.routineIs( _.mapIs ) );
+  test.is( _.routineIs( _.mapExtend ) );
+
+  test.case = 'routines _.str*';
+  test.is( _.routineIs( _.mapIs ) );
+  test.is( _.routineIs( _.mapExtend ) );
+}
+
+// --
+// declare
+// --
+
+var Self =
+{
+
+  name : 'Tools.base.l6.path',
+  silencing : 1,
+
+  tests :
+  {
+
+    routinesOfTools,
+
+  },
+
+}
+
+Self = wTestSuite( Self );
+if( typeof module !== 'undefined' && !module.parent )
+wTester.test( Self.name );
+
+})();
---
+++ proto/dwtools/abase/l3/Include.s
@@ -0,0 +1,27 @@
+//#! /usr/bin/env node
+( function _wtools_s_( ){
+
+'use strict';
+
+/**
+ * Collection of general purpose tools for solving problems. Aggregates module Tools. Fundamentally extend JavaScript without spoiling namespace, so may be used solely or in conjunction with another module of such kind. Tools contain hundreds of routines to operate effectively with Array, SortedArray, Map, RegExp, Buffer, Time, String, Number, Routine, Error and other fundamental types. The module provides advanced tools for diagnostics and errors handling. Use it to have a stronger foundation for the application.
+ * @module Tools/base/tools
+*/
+
+if( typeof module !== 'undefined' && module !== null )
+{
+  let _ = require( '../../Tools.s' );
+}
+
+let _global = _global_;
+let _ = _global_.wTools;
+let Self = _global_.wTools;
+
+// --
+// export
+// --
+
+if( typeof module !== 'undefined' && module !== null )
+module[ 'exports' ] = Self;
+
+})();
